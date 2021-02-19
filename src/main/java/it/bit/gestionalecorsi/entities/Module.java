1afc3ff87e0c881be09eedb1f82d9dfd02192f0b
andmora <and.morabito@gmail.com>
2021-02-19 12:30:50 +0100

    removed property subject from module.

--- src/main/java/it/bit/gestionalecorsi/entities/Module.java
+++ src/main/java/it/bit/gestionalecorsi/entities/Module.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class Module {
 
     private String name;
-    private String subject;
     private Person teacher;
     private int durationHours;
     private List<Lesson> lessons;
